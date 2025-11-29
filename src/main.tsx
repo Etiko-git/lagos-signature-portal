80ef796edfe5675dffc8af416e7d90c010c3f0a1
Etiko Solih Opemiposi <129898960+Etiko-git@users.noreply.github.com>
2025-11-30 00:03:12 +0530

    Update on myID interation

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import './index.css'
-import { LagosSignaturePortalApp } from './App.tsx'
-import LoginPage from './login.tsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+// import './index.css'
+// import { LagosSignaturePortalApp } from './App.tsx'
+// import LoginPage from './login.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/app" element={<LagosSignaturePortalApp />} />
-        <Route path="/" element={<Navigate to="/login" replace />} />
-      </Routes>
-    </Router>
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <Router>
+//       <Routes>
+//         <Route path="/login" element={<LoginPage />} />
+//         <Route path="/app" element={<LagosSignaturePortalApp />} />
+//         <Route path="/" element={<Navigate to="/login" replace />} />
+//       </Routes>
+//     </Router>
+//   </StrictMode>,
+// )
