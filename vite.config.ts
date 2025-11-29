8f848c06aa7e96352fb4c67b66b24fc128c5bdbb
Etiko Solih Opemiposi <129898960+Etiko-git@users.noreply.github.com>
2025-11-30 00:41:04 +0530

    Update on myID interation

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,12 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        app: './app.html'
-      }
-    }
-  }
 })
+
+
 
 
 /// <reference types="vite/client" />
